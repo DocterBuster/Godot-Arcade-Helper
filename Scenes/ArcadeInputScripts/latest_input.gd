afa2c54d48bb72c2c5cf65d0b79084da474fe43a
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2025-09-01 10:18:42 -0400

    Added readme + starting refactor code cleanup

--- Scenes/ArcadeInputScripts/latest_input.gd
+++ Scenes/ArcadeInputScripts/latest_input.gd
@@ -1,26 +1,11 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-
 func _input(event: InputEvent) -> void:
 	
 	if($Delay.is_stopped()):
 		
+		#Set input event as text for visibiility!
 		$InputAction.text = event.as_text()
 		
 		#Start the timer 
 		$Delay.start()
-		
-	
-	
-	
