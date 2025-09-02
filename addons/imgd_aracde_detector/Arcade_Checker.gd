89fad98fb3ba95d1565269dc9432e2b1a4231b53
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2025-09-02 13:16:36 -0400

    Working arcade URL fetching

--- addons/imgd_aracde_detector/Arcade_Checker.gd
+++ addons/imgd_aracde_detector/Arcade_Checker.gd
@@ -1,10 +1,13 @@
 extends Node
 
 var is_on_arcade : bool = false
-
-const arcade_URL : String = "https://api.github.com/repos/godotengine/godot/releases/latest"
+var arcade_URL : String
 
 func _ready() -> void:
+	
+	#Get the URL for the arcade
+	arcade_URL = JavaScriptBridge.eval("window.location.href")
+	
 	#Connect HTTP Signal
 	$HTTPRequest.request_completed.connect(_on_http_request_request_completed)
 	#Request the page info
@@ -14,7 +17,6 @@ func _on_http_request_request_completed(result: int, response_code: int, headers
 	
 	#Fetch JSON data
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json["name"])
 	
 	#Only check if the json file has the arcade key
 	if(json.has("arcade")):
