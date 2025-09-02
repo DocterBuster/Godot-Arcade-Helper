2f69a9f4d9221d4a024cd3216da041e85fb42da6
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2025-09-02 13:46:22 -0400

    Web checker

--- addons/imgd_aracde_detector/Arcade_Checker.gd
+++ addons/imgd_aracde_detector/Arcade_Checker.gd
@@ -6,14 +6,20 @@ var arcade_URL : String
 func _ready() -> void:
 	
 	#Get the URL for the arcade
-	arcade_URL = JavaScriptBridge.eval("window.location.href")
-	
-	#Connect HTTP Signal
-	$HTTPRequest.request_completed.connect(_on_http_request_request_completed)
-	#Request the page info
-	$HTTPRequest.request(arcade_URL)
+	if(OS.has_feature("web")):
+		print("Arcade Checker: Web Build Detected!")
+		#Wait a bit for the project to start
+		await get_tree().create_timer(1.0).timeout
+		arcade_URL = JavaScriptBridge.eval("window.location.href")
+		
+		#Connect HTTP Signal
+		$HTTPRequest.request_completed.connect(_on_http_request_request_completed)
+		#Request the page info
+		$HTTPRequest.request(arcade_URL)
 
 func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+	
+	print(headers)
 	
 	#Fetch JSON data
 	var json = JSON.parse_string(body.get_string_from_utf8())
@@ -24,7 +30,6 @@ func _on_http_request_request_completed(result: int, response_code: int, headers
 		is_on_arcade = true
 	else:
 		is_on_arcade = false
-	
 	
 	#Set Debug Label
 	$DEBUG_DISPLAY/Label.text = str("is_on_arcade = ", is_on_arcade)
