06ed791305b0959ba23c20267a8b93ab30caa297
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2025-09-27 11:48:03 -0400

    Scene change effect

--- Singletons/SubGameLoader.gd
+++ Singletons/SubGameLoader.gd
@@ -8,12 +8,24 @@ extends CanvasLayer
 
 #Loads a subgame from a filepath that points to it's main scene 
 func load_subgame_from_path_to_main(main_path : String):
+	get_tree().unload_current_scene()
+	
+	await scene_transition()
+	
 	get_tree().change_scene_to_file(main_path)
 	in_subgame = true
 
 
 func exit_subgame():
+	get_tree().unload_current_scene()
+	await scene_transition()
 	get_tree().change_scene_to_file("res://Scenes/Main.tscn")
+
+
+func scene_transition():
+	$StaticEffect.visible = true
+	await get_tree().create_timer(0.6).timeout
+	$StaticEffect.visible = false
 
 
 func _process(delta: float) -> void:
